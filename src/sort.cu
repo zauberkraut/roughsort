11b3919da73c5156f430827d3e2b6e055b19f0af
zauberkraut <jet@t520>
2016-10-11 13:53:31 -0600

    added thrust calls to dev sorts

--- src/sort.cu
+++ src/sort.cu
@@ -1,15 +1,20 @@
 /* sort.cu: CUDA kernels for parallel sorts on an Nvidia GPU. */
 
+#include "thrust/device_ptr.h"
+#include "thrust/sort.h"
 #include "roughsort.h"
 
 void devMergesort(int32_t* const a, const int n) {
-  // TODO
+  thrust::device_ptr<int32_t> devA(a);
+  thrust::stable_sort(devA, devA + n);
 }
 
 void devQuicksort(int32_t* const a, const int n) {
-  // TODO
+  thrust::device_ptr<int32_t> devA(a);
+  thrust::sort(devA, devA + n);
 }
 
 void devRoughsort(int32_t* const a, const int n) {
+  thrust::device_ptr<int32_t> devA(a);
   // TODO
 }
